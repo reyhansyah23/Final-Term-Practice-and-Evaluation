33fc02c78f9be0aee66d8e6bf66beea9a2199060
Reyhansyah Prawira <57929589+reyhansyah23@users.noreply.github.com>
2022-01-07 17:45:38 +0700

    Update Net.py

--- code1/Net.py
+++ code1/Net.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from Opts import lrelu, resUnit
 from dataBlocks import DataBlocks
-import pickle
 # import cPickle
+import pickle
 import scipy.io as sio
 import numpy as np
 
@@ -18,7 +18,7 @@ bias_initializer = tf.constant_initializer(0.0)
 
 def build_data(batchsize):
     readpath = open('img.pkl', 'rb')
-    datapaths = cPickle.load(readpath)
+    datapaths = pickle.load(readpath)
     db = DataBlocks(data_paths=datapaths, train_valid_ratio=[39, 0], batchsize=batchsize, allow_preload=False)
     mask_s = np.load('syn_data/mask.npy')
     return db, mask_s
@@ -64,59 +64,76 @@ def generator(image, z):
     n = 64
     with tf.name_scope("generator"):
         # original
-        e1 = slim.conv2d(image, n, [4, 4], 2, activation_fn=lrelu, scope='g_e1_conv',
+        e1 = slim.conv2d(image, n, [4, 4], 2, activation_fn=lrelu,
                          weights_initializer=initializer)
         # 256
-        e2 = slim.conv2d(lrelu(e1), 2 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e2_conv',
+        e2 = slim.conv2d(lrelu(e1), 2 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
                          weights_initializer=initializer)
         # 128
-        e3 = slim.conv2d(lrelu(e2), 4 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e3_conv',
+        e3 = slim.conv2d(lrelu(e2), 4 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
                          weights_initializer=initializer)
         # 64
-        e4 = slim.conv2d(lrelu(e3), 8 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e4_conv',
+        e4 = slim.conv2d(lrelu(e3), 8 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
                          weights_initializer=initializer)
         # 32
-        e5 = slim.conv2d(lrelu(e4), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e5_conv',
+        e5 = slim.conv2d(lrelu(e4), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
                          weights_initializer=initializer)
         # 16
-        e6 = slim.conv2d(lrelu(e5), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e6_conv',
+        e6 = slim.conv2d(lrelu(e5), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
                          weights_initializer=initializer)
+        # e1 = slim.conv2d(image, n, [4, 4], 2, activation_fn=lrelu, scope='g_e1_conv',
+        #                  weights_initializer=initializer, reuse=True)
+        # # 256
+        # e2 = slim.conv2d(lrelu(e1), 2 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e2_conv',
+        #                  weights_initializer=initializer,reuse=True)
+        # # 128
+        # e3 = slim.conv2d(lrelu(e2), 4 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e3_conv',
+        #                  weights_initializer=initializer,reuse=True)
+        # # 64
+        # e4 = slim.conv2d(lrelu(e3), 8 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e4_conv',
+        #                  weights_initializer=initializer,reuse=True)
+        # # 32
+        # e5 = slim.conv2d(lrelu(e4), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e5_conv',
+        #                  weights_initializer=initializer,reuse=True)
+        # # 16
+        # e6 = slim.conv2d(lrelu(e5), 8*n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None, scope='g_e6_conv',
+        #                  weights_initializer=initializer,reuse=True)
 
 
 
         zP = slim.fully_connected(z, 4 * 4 * n, normalizer_fn=None, activation_fn=lrelu, scope='g_project',
-                                  weights_initializer=initializer)
+                                  weights_initializer=initializer,reuse=True)
         zCon = tf.reshape(zP, [-1, 4, 4, n])
 
         gen1 = slim.conv2d_transpose(lrelu(zCon), 2 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv1', weights_initializer=initializer)
+                                     scope='g_dconv1', weights_initializer=initializer,reuse=True)
         # 8
         gen1 = tf.concat(3, [gen1, e6])
         
         gen2 = slim.conv2d_transpose(lrelu(gen1), 4 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv2', weights_initializer=initializer)
+                                     scope='g_dconv2', weights_initializer=initializer,reuse=True)
         # 16
         gen2 = tf.concat(3, [gen2, e5])
 
         gen3 = slim.conv2d_transpose(lrelu(gen2), 8 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv3', weights_initializer=initializer)
+                                     scope='g_dconv3', weights_initializer=initializer,reuse=True)
         gen3 = tf.concat(3, [gen3, e4])
 
         # 32
         gen6 = slim.conv2d_transpose(tf.nn.relu(gen3), 4 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv6', weights_initializer=initializer)
+                                     scope='g_dconv6', weights_initializer=initializer,reuse=True)
         gen6 = tf.concat(3, [gen6, e3])
 
         # 64
         gen7 = slim.conv2d_transpose(tf.nn.relu(gen6), 2 * n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv7', weights_initializer=initializer)
+                                     scope='g_dconv7', weights_initializer=initializer,reuse=True)
         gen7 = tf.concat(3, [gen7, e2])
 
         # 128
         gen8 = slim.conv2d_transpose(tf.nn.relu(gen7), n, [4, 4], 2, normalizer_fn=slim.batch_norm, activation_fn=None,
-                                     scope='g_dconv8', weights_initializer=initializer)
+                                     scope='g_dconv8', weights_initializer=initializer,reuse=True)
         # gen8 = tf.nn.dropout(gen8, 0.5)
-        gen8 = tf.concat(3, [gen8, e1])
+        gen8 = tf.concat([gen8, e1],3)
         gen8 = tf.nn.relu(gen8)
 
         # 256
@@ -124,4 +141,3 @@ def generator(image, z):
                                         weights_initializer=initializer)
 
     return gen_out    
-    
